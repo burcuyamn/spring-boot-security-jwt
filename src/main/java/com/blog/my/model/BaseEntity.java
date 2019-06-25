6906b8296c899cc0e50005bc84e96cf073a1df24
burcuyaman <burcu.yaman@gvg.com.tr>
2019-06-25 10:18:56 +0300

    removed the lombok

--- src/main/java/com/blog/my/model/BaseEntity.java
+++ src/main/java/com/blog/my/model/BaseEntity.java
@@ -1,7 +1,5 @@
 package com.blog.my.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
@@ -9,8 +7,6 @@ import java.io.Serializable;
 import java.util.Date;
 
 @MappedSuperclass
-@Getter
-@Setter
 public class BaseEntity implements Serializable {
     @Id
     @GenericGenerator(name = "system-uuid", strategy = "uuid")
@@ -25,4 +21,28 @@ public class BaseEntity implements Serializable {
 
     @Column(name = "DELETED")
     private Date deleted;
+
+    public String getOid() {
+        return oid;
+    }
+
+    public void setOid(String oid) {
+        this.oid = oid;
+    }
+
+    public Date getLastUpdated() {
+        return lastUpdated;
+    }
+
+    public void setLastUpdated(Date lastUpdated) {
+        this.lastUpdated = lastUpdated;
+    }
+
+    public Date getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Date deleted) {
+        this.deleted = deleted;
+    }
 }
--- src/main/java/com/blog/my/model/User.java
+++ src/main/java/com/blog/my/model/User.java
@@ -1,7 +1,5 @@
 package com.blog.my.model;
 
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -9,7 +7,6 @@ import javax.persistence.*;
 @Entity
 @Table(name = "USERS")
 @EntityListeners(AuditingEntityListener.class)
-@Getter @Setter
 public class User extends BaseEntity {
 
     @Column(name = "USERNAME")
@@ -20,4 +17,36 @@ public class User extends BaseEntity {
 
     @Column(name = "PASSWORD")
     private String password;
+
+    public User(){}
+
+    public User(String username, String email, String password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
