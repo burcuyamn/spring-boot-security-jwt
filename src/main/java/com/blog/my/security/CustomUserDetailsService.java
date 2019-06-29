6e09e78de8f3bd154485f7287c88109ee57d2274
burcuyaman <burcu.yaman@gvg.com.tr>
2019-06-29 23:54:22 +0300

    added user principal

--- src/main/java/com/blog/my/security/CustomUserDetailsService.java
+++ src/main/java/com/blog/my/security/CustomUserDetailsService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -15,6 +16,12 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userService.findByUsername(username);
-        return null;
+        return UserPrincipal.create(user);
+    }
+
+    @Transactional
+    public UserDetails loadUserById(String oid) {
+        User user = userService.findById(oid);
+        return UserPrincipal.create(user);
     }
 }
